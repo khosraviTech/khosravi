25565ec2f59d8c25db7fb941b7007fa869312405
khosraviTech <khosravitech@gmail.com>
2026-09-13 18:22:18 +0330

    add Scalar tag to project.

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -18,35 +18,36 @@ const projects: project[] = [
     address: "https://nextkala.vercel.app/",
     image: "/projectImages/NEXTKALA2.JPG",
     gitHubAddress: "https://github.com/khosraviTech/nextkala",
-    demo:true
+    demo: true
   },
-   {
-  title: "NextKALA API",
-  description:
-    "A RESTful e-commerce backend built with FastAPI, PostgreSQL, SQLAlchemy, Pydantic, and Alembic, following a clean Repository-Service-Router architecture.",
-  tags: [
-    "FastAPI",
-    "Python",
-    "PostgreSQL",
-    "SQLAlchemy",
-    "Pydantic",
-    "Alembic",
-  ],
-  address: "",
-  image: "/projectImages/NEXTKALA2.JPG",
-  gitHubAddress: "https://github.com/khosraviTech/nextkalaAPI",
-  demo: false,
-}
-  
+  {
+    title: "NextKALA API",
+    description:
+      "An e-commerce backend built with FastAPI, PostgreSQL, SQLAlchemy, Pydantic, and Alembic, following a clean Repository-Service-Router architecture.",
+    tags: [
+      "FastAPI",
+      "Python",
+      "PostgreSQL",
+      "SQLAlchemy",
+      "Pydantic",
+      "Alembic",
+      "Scalar"
+    ],
+    address: "",
+    image: "/projectImages/image2.png",
+    gitHubAddress: "https://github.com/khosraviTech/nextkalaAPI",
+    demo: false,
+  }
+
 ];
 
 export default function Projects() {
   return (
     <>
       <div className="border-2 border-[#B7B0A8]  shadow-xl rounded-3xl p-4 grid grid-cols-12 gap-6 bg-bg-card">
-       
-       
-       
+
+
+
         <div className="col-span-12 inline-flex items-center gap-4 ml-2">
           <FolderClosed className="text-accent-green scale-150" />
           <h1 className="text-primary text-3xl"> Featured Projects</h1>
@@ -56,13 +57,13 @@ export default function Projects() {
           {projects.map((project) => (
             <div
               key={project.title}
-              className="grid grid-cols-12 text-primary bg-bg-card   rounded-3xl p-3 gap-2"
+              className="w-170 grid grid-cols-12 text-primary bg-bg-card  rounded-3xl p-3 gap-2"
             >
               {/* project image */}
               <Image
                 src={project.image}
-                width={600}
-                height={600}
+                width={400}
+                height={400}
                 alt="Picture of the project"
                 className=" col-span-12 rounded-2xl  pl-5 m-0"
               />
@@ -84,16 +85,18 @@ export default function Projects() {
                   </div>
                 ))}
               </div>
-              
-              
-             {project.demo === true && (
-  <Link
-    href={project.address}
-    className="hover:bg-[#57cc99] border-2 hover:border-[#57cc99] col-span-6 rounded-xl inline-flex items-center gap-2 justify-between p-4 m-2 font-medium"
-  >
-    Live Demo <MonitorPlay className="scale-120" />
-  </Link>
-)}
+
+
+              {project.demo === true && (
+                <Link
+                  href={project.address}
+                  className="hover:bg-[#57cc99] border-2 hover:border-[#57cc99] col-span-6 rounded-xl inline-flex items-center gap-2 justify-between p-4 m-2 font-medium"
+                >
+                  Live Demo <MonitorPlay className="scale-120" />
+                </Link>
+              )}
+
+
 
 
 
@@ -111,6 +114,7 @@ export default function Projects() {
                 />
               </Link>
             </div>
+
           ))}
         </div>
       </div>
